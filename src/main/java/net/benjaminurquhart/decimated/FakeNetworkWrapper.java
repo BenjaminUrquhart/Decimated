3ba0c4613a05b4aca54b8bf89c2a3f5cb8adb4fb
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2019-08-09 11:25:37 -0400

    We don't have to drop the packet, we can edit it instead

--- src/main/java/net/benjaminurquhart/decimated/FakeNetworkWrapper.java
+++ src/main/java/net/benjaminurquhart/decimated/FakeNetworkWrapper.java
@@ -1,5 +1,8 @@
 package net.benjaminurquhart.decimated;
 
+import java.io.File;
+import java.util.List;
+
 import com.boehmod.lib.utils.BoehModLogger;
 import com.boehmod.lib.utils.BoehModLogger.EnumLogType;
 
@@ -8,10 +11,12 @@ import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.decimation.mod.utilities.net.messages_minecraft.Message_Cheating;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.Packet;
+import java.util.Arrays;
 
 public class FakeNetworkWrapper extends SimpleNetworkWrapper {
 
 	private SimpleNetworkWrapper real;
+	public static final List<String> FILENAMES = Arrays.asList("Decimation.jar","DecimationVoiceChat.jar");
 	
 	public FakeNetworkWrapper() {
 		super("ctx");
@@ -37,9 +42,22 @@ public class FakeNetworkWrapper extends SimpleNetworkWrapper {
     public void sendToServer(IMessage message) {
     	if(message instanceof Message_Cheating) {
     		Message_Cheating msg = (Message_Cheating) message;
-    		BoehModLogger.printLine(EnumLogType.ANTICHEAT, "Dropped cheating response (isCheating=" + msg.isCheating + ", directorySize=" + msg.directorySize + ")");
-    		return;
+    		BoehModLogger.printLine(EnumLogType.ANTICHEAT, "Intercepted cheating response");
+    		BoehModLogger.printLine(EnumLogType.ANTICHEAT, "Cheating: " + msg.isCheating);
+    		BoehModLogger.printLine(EnumLogType.ANTICHEAT, "Directory size: " + msg.directorySize);
+    		msg.isCheating = false;
+    		msg.directorySize = calculateSize(new File("mods/"));
+    		BoehModLogger.printLine(EnumLogType.ANTICHEAT, "New Directory size: " + msg.directorySize);
     	}
     	real.sendToServer(message);
     }
+	private long calculateSize(File file) {
+		long size = 0L;
+		for(File f : file.listFiles()) {
+			if(f.isFile() && FILENAMES.contains(f.getName())) {
+				size += f.length();
+			}
+		}
+		return size;
+	}
 }
